94c6c043ed4bfbff79ac7b34a7cf22f8f5cd7f42
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 12:29:38 -0700

    Wrap Image comp. in TouWF comp. to handle touch/press events

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
-      <Image
-        source={{
-          width: 200, // specifying width and height is necessary for remote URIs
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200, // specifying width and height is necessary for remote URIs
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
