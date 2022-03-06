3ec33e9a8fab14641b79d03195f241306b6ae038
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 12:15:54 -0700

    Adjust style to center Text component and align items

--- App.js
+++ App.js
@@ -17,6 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
