6cd5e7a9b679978ef424f75fc21b84db27294277
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 12:15:01 -0700

    use numberOfLines and onPress prop on Text component

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World - A really really long text. Now I wanna make this even
+        longer and see what happens.
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
