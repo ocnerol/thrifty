8d869b08ef2de8a88c239fd692d0d154c8fbcbeb
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 12:22:58 -0700

    use remote URI image source and specify dimensions

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
-      <Image source={require("./assets/icon.png")} />
+      <Image
+        source={{
+          width: 200, // specifying width and height is necessary for remote URIs
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
