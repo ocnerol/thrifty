75ec2622c680a1665dd5c58a5ced8f8054292073
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 12:20:52 -0700

    Add Image component which uses require to get local image

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
+      <Image source={require("./assets/icon.png")} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
