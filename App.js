7d446e8c9afef1917f0d4f2d717415e895244acf
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-06 19:14:40 -0700

    Clean up code and import Button component

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableNativeFeedback,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <View
-          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
-        ></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button />
     </SafeAreaView>
   );
 }
