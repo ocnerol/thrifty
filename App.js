24fc4a7b77227b2c3b47fc0d53c48d1ad608af15
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-08 17:09:29 -0700

    conditionally set width of View component based on orientation

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ import {
   StatusBar,
   View,
 } from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  console.log(`${Platform.OS} ${JSON.stringify(useDimensions())}`);
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: "100%", // percentage of screen width
-          height: "30%", // percentage of screen width
+          height: landscape ? "100%" : "30%",
         }}
       ></View>
     </SafeAreaView>
