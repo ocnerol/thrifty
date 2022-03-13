d7565890cfc6af224df15e2478cb0abe9ead4d71
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 13:39:22 -0600

    use alignContent to align entire content as a whole along secondary axis

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default function App() {
         alignItems: "center", // secondary axis
         flexWrap: "wrap", // when we enable wrapping, the alignItems property
         // behaves differently: it aligns the items along the secondary axis within each line
+        alignContent: "center",
       }}
     >
       <View
@@ -46,13 +47,6 @@ export default function App() {
           backgroundColor: "greenyellow",
           width: 100,
           height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "red",
-          width: 100,
-          height: 400,
         }}
       />
     </View>
