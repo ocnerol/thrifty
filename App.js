fdaa690a6bfdb12d83bd935bd11708d9779e1096
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 14:45:41 -0600

    return WelcomeScreen component

--- App.js
+++ App.js
@@ -1,40 +1,6 @@
 import { View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", // main axis
-        alignItems: "center", // secondary axis
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
