3d6e2795060ca9a9a5e0104659a5043b416c9cd0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-12 19:28:12 -0700

    Use flex: 1 to take available free space

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
-import {
-  StyleSheet,
-  SafeAreaView,
-  Button,
-  Platform,
-  StatusBar,
-  View,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+      }}
+    ></View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
