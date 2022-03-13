46a55e242310bc4376affa015b5c2c031ab9da80
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 17:14:42 -0600

    Setup styles for ViewImageScreen

---
+++ screens/ViewImageScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+export default function ViewImageScreen() {
+  return;
+}
+
+const styles = StyleSheet.create({
+  background: {},
+  buttons: {},
+  image: {},
+});
