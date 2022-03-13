9cac3d63259445bca63fbc1df57e9b943dd06cac
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 17:17:22 -0600

    Render ViewImageScreen component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import ViewImageScreen from "./screens/ViewImageScreen";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <WelcomeScreen />
+      {/* <WelcomeScreen /> */}
+      <ViewImageScreen />
     </View>
   );
 }
