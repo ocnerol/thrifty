d8a9061d73196a0b79b3b6c307cdca2aeb67b5f1
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 17:21:53 -0600

    Add styles for icon placeholders

--- screens/ViewImageScreen.js
+++ screens/ViewImageScreen.js
@@ -1,6 +1,14 @@
 import { View, StyleSheet } from "react-native";
 export default function ViewImageScreen() {
-  return <View style={styles.background}></View>;
+  return (
+    <View style={styles.background}>
+      <View style={styles.buttons}>
+        <View style={styles.closeButton} />
+        <View style={styles.deleteButton} />
+      </View>
+      <View />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -8,6 +16,18 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     flex: 1,
   },
-  buttons: {},
+  buttons: {
+    flexDirection: "row",
+  },
+  closeButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+  },
+  deleteButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ECDC4",
+  },
   image: {},
 });
