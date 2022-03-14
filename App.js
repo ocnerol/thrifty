946190849232ef63e71fe27114c31dad0ce359cf
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-13 23:22:40 -0600

    render ViewImageScreen to see screen while refactoring

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
-  {
-    /* <ViewImageScreen /> */
-  }
+  return <ViewImageScreen />;
+  // <WelcomeScreen />;
 }
