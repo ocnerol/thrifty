1130da96138c632af58faec297c783ac0f328b6c
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 20:43:01 -0600

    use Platform.select to use platform specific component

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,25 +1,4 @@
-import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
-
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
-}
-
-const styles = StyleSheet.create({
-  fontSize: 18,
-  text: {
-    color: "tomato",
-    fontWeight: "600",
-    ...Platform.select({
-      ios: {
-        fontSize: 30,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 28,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
+export default AppText = Platform.select({
+  ios: () => require("AppText.ios.js"),
+  android: () => require("AppText.android.js"),
 });
-export default AppText;
