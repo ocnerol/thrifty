6947da48e57cd2942b96931c145c43e589907339
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 19:59:34 -0600

    use icon from MaterialCommunityIcons and apply style rules

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import { View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 
@@ -13,7 +14,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <AppText>Hola pops!</AppText>
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
     </View>
   );
 }
