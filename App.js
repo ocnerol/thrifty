0c57be45972b087d2d360ae42b24c87a34c29376
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 19:24:13 -0600

    Add properties to make shadow behind View for iOS - shadowColor, shadowOffset, and shadowOpacity are necessary for making a shadow in iOS

--- App.js
+++ App.js
@@ -16,9 +16,10 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50, // use border of at least half the height or width to get a circle
+          shadowColor: "grey",
+          shadowOffset: { width: -10, height: 10 }, // angle or position of shadow
+          shadowOpacity: 1, // 0 no shadow, 1 really dark
+          shadowRadius: 10,
         }}
       ></View>
     </View>
