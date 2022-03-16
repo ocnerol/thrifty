e1a96df2ea0e2ed7384f9127d954d191b445c3d4
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 20:56:06 -0600

    update import path

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import { View, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import AppText from "./app/components/AppText";
+import AppText from "./app/components/AppText/AppText";
 
 export default function App() {
   return (
