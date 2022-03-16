99c43aef4ffe68ee5015523741775fdd483343e9
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 19:21:22 -0600

    use borderRadius to make View a circle

--- App.js
+++ App.js
@@ -16,6 +16,9 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50, // use border of at least half the height or width to get a circle
         }}
       ></View>
     </View>
