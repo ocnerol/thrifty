573fde7f78abe513ef3007f70841bb6d90099397
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-15 19:55:03 -0600

    use AppText component

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import { View, Text } from "react-native";
 
+import AppText from "./app/components/AppText";
+
 export default function App() {
   return (
     <View
@@ -11,20 +13,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text
-        style={{
-          fontSize: 30,
-          fontStyle: "italic",
-          fontWeight: "600", // has to be a string
-          color: "tomato",
-          textTransform: "capitalize",
-          textAlign: "right",
-          lineHeight: 30, // space between lines
-        }}
-      >
-        I love React Native! This is my first React Native App! Here is some
-        more text.
-      </Text>
+      <AppText>Hola pops!</AppText>
     </View>
   );
 }
