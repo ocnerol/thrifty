755c191eb2cb7884d9f96eddcf37113919c73d0b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-16 19:57:51 -0600

    Design ListingDetailsScreen component

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,46 @@
+import { View, StyleSheet, Image, Text } from "react-native";
+
+export default function ListingDetailsScreen(props) {
+  return (
+    <View style={styles.container}>
+      {props.children}
+      <View style={styles.sellerContainer}>
+        <Image
+          style={styles.sellerPhoto}
+          source={require("../assets/mosh.jpg")}
+        />
+        <View style={styles.sellerInfo}>
+          <Text style={styles.sellerName}>Mosh Hamedani</Text>
+          <Text style={styles.sellerListingCount}>5 Listings</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sellerContainer: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+  },
+  sellerPhoto: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+  },
+  sellerInfo: {
+    padding: 5,
+  },
+  sellerName: {
+    fontSize: 16,
+    fontWeight: "300",
+  },
+  sellerListingCount: {
+    paddingTop: 5,
+    fontSize: 15,
+    color: "grey",
+  },
+});
