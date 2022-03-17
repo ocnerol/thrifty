771a44182b0c23fc6f08dfa17a9b1cabff200b0f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-16 19:36:21 -0600

    Render Card component for design testing

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import { View, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 import WelcomeScreenButton from "./app/components/WelcomeScreenButton/WelcomeScreenButton";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        height: 400,
+        backgroundColor: "#f8f4f4",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingTop: 80,
+      }}
+    >
+      <Card
+        title="Red jacket for sale!"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
