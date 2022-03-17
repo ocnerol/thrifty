50571246483bbc355f42ab4a5d4e7e583b9bcbc7
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-16 20:34:20 -0600

    use array of style objects to dynamically set styles using prop values

--- app/components/AppButton/AppButton.js
+++ app/components/AppButton/AppButton.js
@@ -2,29 +2,30 @@ import { Text, StyleSheet, TouchableOpacity } from "react-native";
 
 import colors from "../../config/colors";
 
-function AppButton({ color, title, onPress }) {
-  const styles = StyleSheet.create({
-    button: {
-      backgroundColor: colors[color],
-      width: "90%",
-      alignItems: "center",
-      borderRadius: 25,
-      paddingVertical: 12,
-      marginVertical: 6,
-    },
-    text: {
-      color: colors.white,
-      fontWeight: "bold",
-      textTransform: "uppercase",
-      fontSize: 18,
-    },
-  });
-
+function AppButton({ color = "primary", title, onPress }) {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 }
 
+const styles = StyleSheet.create({
+  button: {
+    width: "90%",
+    alignItems: "center",
+    borderRadius: 25,
+    paddingVertical: 12,
+    marginVertical: 6,
+  },
+  text: {
+    color: colors.white,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 18,
+  },
+});
 export default AppButton;
