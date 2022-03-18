d539bd928c19c8f48c81c80393eea72c9dcb191e
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-17 18:46:42 -0600

    Add listing details components and styles

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,9 +1,16 @@
 import { View, StyleSheet, Image, Text } from "react-native";
 
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+
 export default function ListingDetailsScreen(props) {
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
       <View style={styles.sellerContainer}>
         <Image
           style={styles.sellerPhoto}
@@ -25,6 +32,19 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
   },
   sellerContainer: {
     paddingHorizontal: 20,
