d166b1acb8c03d5eb9ef35943563308f961326a8
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-18 16:00:58 -0600

    add ability to dynamically set background color

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,8 +1,12 @@
 import { StyleSheet, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
 
-export default function Screen({ children }) {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+export default function Screen({ children, color }) {
+  return (
+    <SafeAreaView style={[styles.screen, { backgroundColor: color }]}>
+      {children}
+    </SafeAreaView>
+  );
 }
 const styles = StyleSheet.create({
   screen: {
