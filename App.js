a918d66d5ad5c7471c66655ed58d4517c815f974
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-18 16:39:43 -0600

    render ListingsScreen

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import MyAccountScreen from "./app/screens/MyAccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <MyAccountScreen />;
+  return <ListingsScreen />;
 }
