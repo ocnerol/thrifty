89fc70a549929923c845f64e2a79a4c3a71248c1
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-18 16:59:23 -0600

    refactor to use FlatList

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,23 +1,77 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 import Card from "../components/Card";
+import ListItemSeparator from "../components/ListItemSeparator";
 import Screen from "../components/Screen";
+import colors from "../config/colors";
+
+const initialListings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    subTitle: "$100",
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    subTitle: "$1000",
+    image: require("../assets/couch.jpg"),
+  },
+];
 
 export default function ListingsScreen() {
+  const [listings, setListings] = useState(initialListings);
+  const [refreshing, setRefreshing] = useState(false);
   return (
-    <Screen>
-      <View style={styles.container}>
-        <Card
-          title={"Red jacket for sale"}
-          subTitle={"$100"}
-          image={require("../assets/jacket.jpg")}
-        />
-      </View>
+    <Screen style={styles.screen}>
+      <FlatList
+        style={styles.container}
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={item.subTitle}
+            image={item.image}
+            onPress={() => console.log("Listing selected", item)}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setListings([
+            {
+              id: 3,
+              title: "T3",
+              description: "T3",
+              image: require("../assets/mosh.jpg"),
+            },
+            {
+              id: 4,
+              title: "T4",
+              description: "T4",
+              image: require("../assets/mosh.jpg"),
+            },
+            {
+              id: 5,
+              title: "T5",
+              description: "T5",
+              image: require("../assets/mosh.jpg"),
+            },
+          ]);
+        }}
+      />
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.light,
+  },
   container: {
     padding: 20,
   },
