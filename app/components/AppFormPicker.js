eb8c61010cae712126e2801e8821b9472866345b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 16:17:42 -0600

    Set up AppPicker component using Formik context

--- app/components/AppFormPicker.js
+++ app/components/AppFormPicker.js
@@ -1,8 +1,22 @@
 import { StyleSheet } from "react-native";
+import { useFormikContext } from "formik";
+
 import AppPicker from "./AppPicker";
 
-export default function AppFormPicker() {
-  return <AppPicker />;
+export default function AppFormPicker({ items, name, placeholder }) {
+  const { setFieldValue, values } = useFormikContext();
+  console.log("values:", values);
+  console.log("values[name]:", values[name]);
+  console.log("name:", name);
+
+  return (
+    <AppPicker
+      onSelectItem={(item) => setFieldValue(name, item)}
+      placeholder={placeholder}
+      items={items}
+      selectedItem={values[name]}
+    />
+  );
 }
 
 const styles = StyleSheet.create({});
