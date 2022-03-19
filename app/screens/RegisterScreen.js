51cbf0068e50aa645938b66542911f597c500a2d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 14:39:51 -0600

    remove logo and rename submit button title

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,55 @@
+import { Image, StyleSheet } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"), // label method takes label for displaying errors on this input field
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+export default function RegisterScreen() {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Register" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
