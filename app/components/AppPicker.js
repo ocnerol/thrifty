f453cf87bae95348e758a683a5bdc853450232d8
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 16:40:30 -0600

    add color for placeholder in AppPicker component

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -41,9 +41,12 @@ const AppPicker = ({
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
+
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -84,6 +87,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
+    flex: 1,
   },
   text: {
     flex: 1,
