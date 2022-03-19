c2dfc36bef53987a1704c344d2bb8093333cb3c4
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 16:22:24 -0600

    update import/export to include AppFormPicker

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -2,8 +2,12 @@ import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
-import { AppForm, AppFormField, SubmitButton } from "../components/forms";
-import AppFormPicker from "../components/forms/AppFormPicker";
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   category: Yup.object().nullable().required().label("Category"),
