097cf5dbd202e41f60b6038960caf834d0f33225
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 15:04:10 -0600

    create initial design of AppFormPicker

---
+++ app/components/AppFormPicker.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import AppPicker from "./AppPicker";
+
+export default function AppFormPicker() {
+  return <AppPicker />;
+}
+
+const styles = StyleSheet.create({});
