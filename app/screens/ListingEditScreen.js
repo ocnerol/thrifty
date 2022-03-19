9d8df30fb5857be65fd4f7842bc7e5586ac1532f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 16:32:48 -0600

    refactor; remove unnecessary props

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -44,38 +44,24 @@ export default function ListingEditScreen() {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="default"
-          maxLength={255}
-          name="title"
-          placeholder="Title"
-          textContentType="none"
-        />
-        <AppFormField
-          autoCapitalize="none"
-          autoCorrect={false}
           keyboardType="numeric"
           maxLength={8}
           name="price"
           placeholder="Price"
-          textContentType="none"
         />
         <AppFormPicker
-          name="category"
           items={categories}
+          name="category"
           placeholder="Category"
         />
         <AppFormField
-          autoCapitalize="none"
-          autoCorrect={true}
-          keyboardType="default"
           maxLength={255}
+          multiline
           name="description"
           numberOfLines={3}
           placeholder="Description"
-          textContentType="none"
         />
         <SubmitButton title="Post" />
       </AppForm>
