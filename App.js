e7424d52b1cdf8fbb5c9101a998d9749a41174c2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 10:44:14 -0600

    add dummy categories data

--- App.js
+++ App.js
@@ -17,13 +17,28 @@ import ListItem from "./app/components/ListItem";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 
+const categories = [
+  {
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    label: "Clothing",
+    value: 2,
+  },
+  {
+    label: "Cameras",
+    value: 3,
+  },
+];
+
 export default function App() {
   const [firstName, setFirstName] = useState("");
   const [isNew, setIsNew] = useState(false);
   console.log(firstName);
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="category" />
+      <AppPicker icon="apps" placeholder="category" items={categories} />
       <AppTextInput icon="email" placeholder="email" />
     </Screen>
   );
