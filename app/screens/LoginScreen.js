460fa88e1721116ed1f383f48bdc5ab01c1842e5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 13:15:35 -0600

    use AppFormField component and pass necessary name prop

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,7 +3,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 
 import AppButton from "../components/AppButton";
-import AppTextInput from "../components/AppTextInput";
+import AppFormField from "../components/AppFormField";
 import ErrorMessage from "../components/ErrorMessage";
 import Screen from "../components/Screen";
 
@@ -23,28 +23,24 @@ export default function LoginScreen() {
       >
         {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
-              onBlur={() => setFieldTouched("email")}
-              onChangeText={handleChange("email")} // "email" comes from name of key in initialValues obj
+              name="email"
               placeholder="Email"
               textContentType="emailAddress"
             />
-            <ErrorMessage error={errors.email} visible={touched.email} />
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
-              onBlur={() => setFieldTouched("password")}
-              onChangeText={handleChange("password")} // "password" comes from name of key in initialValues obj
+              name="password"
               placeholder="Password"
               secureTextEntry
               textContentType="password"
             />
-            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
