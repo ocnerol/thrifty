df78e0591030b4834c1deb2648f49e1a1ca785a7
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 12:07:24 -0600

    add View to add custom styles since SafeAreaView overrides some custom styles

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,8 +1,12 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 import Constants from "expo-constants";
 
 export default function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 const styles = StyleSheet.create({
   screen: {
