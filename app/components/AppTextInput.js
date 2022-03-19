8a0c7d2f8d11f38810ff26afba4318e8974fd424
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 10:04:43 -0600

    use colors from defaultStyles object

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from "../config/colors";
 import defaultStyles from "../config/styles";
 
 const AppTextInput = ({ icon, ...otherProps }) => {
@@ -11,7 +10,7 @@ const AppTextInput = ({ icon, ...otherProps }) => {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
@@ -24,7 +23,7 @@ export default AppTextInput;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
