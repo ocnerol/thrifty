aa2924ffc9659654bcc44ba440dfd6cda1b8692b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 20:39:22 -0600

    initialize CategoryPickerItem

---
+++ app/components/CategoryPickerItem.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function CategoryPickerItem() {
+  return null;
+}
+
+const styles = StyleSheet.create({});
