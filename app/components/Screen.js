bdd37451ca279b92330803aa605e8f722979e7e3
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 20:26:54 -0600

    move style to styles object

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -4,13 +4,16 @@ import Constants from "expo-constants";
 export default function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={[{ flex: 1 }, style]}>{children}</View>
+      <View style={[styles.view, style]}>{children}</View>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
     flex: 1,
   },
 });
