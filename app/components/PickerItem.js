667f7376816525b08de33a7c9b073236723d3db6
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 20:09:41 -0600

    add grid style prop and design

--- app/components/PickerItem.js
+++ app/components/PickerItem.js
@@ -1,17 +1,40 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
-import AppText from "./AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function PickerItem({ label, onPress }) {
+import AppText from "./AppText";
+import defaultStyles from "../config/styles";
+import Icon from "./Icon";
+
+export default function PickerItem({ label, onPress, icon, gridStyle }) {
   return (
     <TouchableOpacity onPress={onPress}>
-      <AppText style={styles.text}>{label}</AppText>
+      {gridStyle ? (
+        <View style={styles.gridContainer}>
+          <View>
+            <Icon
+              name={icon}
+              size={70}
+              backgroundColor={defaultStyles.colors.primary}
+            />
+          </View>
+          <AppText style={styles.text}>{label}</AppText>
+        </View>
+      ) : (
+        <AppText style={styles.text}>{label}</AppText>
+      )}
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  gridContainer: {
+    marginHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   text: {
     padding: 20,
+    width: "100%",
+    flexWrap: "wrap",
   },
 });
