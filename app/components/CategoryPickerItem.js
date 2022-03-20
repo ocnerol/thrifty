9b5945b0b10fb6df4cb8b0ba091ac6327f3b2406
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 20:48:51 -0600

    render Icon and label

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,8 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Icon from "./Icon";
+import AppText from "./AppText";
 
-export default function CategoryPickerItem() {
-  return null;
+export default function CategoryPickerItem({ item, onPress }) {
+  return (
+    <View style={styles.container}>
+      <Icon backgroundColor={item.color} name={item.icon} size={80} />
+      <AppText>{item.label}</AppText>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
