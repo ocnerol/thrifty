963171e2aa883b62b27b9cfe3a29f7dc2d2c5130
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 20:55:05 -0600

    add styles for options container and label

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -7,9 +7,18 @@ export default function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
       <Icon backgroundColor={item.color} name={item.icon} size={80} />
-      <AppText>{item.label}</AppText>
+      <AppText style={styles.label}>{item.label}</AppText>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+  },
+  label: {
+    marginTop: 5,
+  },
+});
