c998a575342eefcdd8cd2ee3434820985f68ec91
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 21:11:14 -0600

    rename AppForm filename

--- app/components/forms/index.js
+++ app/components/forms/index.js
@@ -1,4 +1,4 @@
-export { default as AppForm } from "./AppForm";
+export { default as Form } from "./Form";
 export { default as AppFormField } from "./AppFormField";
 export { default as AppFormPicker } from "./AppFormPicker";
 export { default as ErrorMessage } from "./ErrorMessage";
