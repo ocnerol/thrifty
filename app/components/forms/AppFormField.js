7f5ecd9fd362137ecf40643339fc85beb57afe91
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-19 21:10:31 -0600

    rename AppTextInput filename

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -1,14 +1,14 @@
 import { StyleSheet } from "react-native";
 import { useFormikContext } from "formik";
 
-import AppTextInput from "../AppTextInput";
+import TextInput from "../TextInput";
 import ErrorMessage from "./ErrorMessage";
 
 export default function AppFormField({ name, ...otherProps }) {
   const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
   return (
     <>
-      <AppTextInput
+      <TextInput
         onBlur={() => setFieldTouched(name)}
         onChangeText={handleChange(name)} // "email" comes from name of key in initialValues obj
         {...otherProps}
