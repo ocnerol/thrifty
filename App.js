83fbfd197933cbba36fbe23b0b8e2a166dc22c42
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-21 14:19:05 -0600

    use Permissions module to request multiple permissions - note: result.granted will be true iff all requested permissions within result object were granted

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access the library.");
+    const result = await Permissions.askAsync(
+      Permissions.MEDIA_LIBRARY,
+      Permissions.LOCATION_BACKGROUND
+    );
+    if (!result.granted)
+      alert(
+        "You need to enable permission to access the media library and location."
+      );
   };
   useEffect(() => {
     requestPermission();
