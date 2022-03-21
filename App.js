31d6aa5bcc172eb7e3346092430ba98056bef3f2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-21 15:36:46 -0600

    cleanup

--- App.js
+++ App.js
@@ -1,36 +1,17 @@
-import { useEffect, useState } from "react";
-import { Button, Image } from "react-native";
-import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 import Screen from "./app/components/Screen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
   const [imageUri, setImageUri] = useState();
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted)
-      alert(
-        "You need to enable permission to access the media library and location."
-      );
-  };
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error reading an image", error);
-    }
-  };
-
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
