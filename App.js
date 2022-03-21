9282964788c950b7315224be09006c35b6042601
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-21 14:15:18 -0600

    get permission to access device media library using ImagePicker module

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return <Screen></Screen>;
 }
