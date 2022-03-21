aa6c15602ba59bf965a7c081f260f2fffa9deab2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-21 14:20:45 -0600

    revert back to using ImagePicker module

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const requestPermission = async () => {
-    const result = await Permissions.askAsync(
-      Permissions.MEDIA_LIBRARY,
-      Permissions.LOCATION_BACKGROUND
-    );
-    if (!result.granted)
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
       alert(
         "You need to enable permission to access the media library and location."
       );
