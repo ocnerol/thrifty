5cec01c6b1646854cde90d4579f0f12ef9074592
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-22 17:43:06 -0600

    implement a single stack navigator

--- App.js
+++ App.js
@@ -1,5 +1,40 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Screen from "./app/components/Screen";
+
+const Tweets = () => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
