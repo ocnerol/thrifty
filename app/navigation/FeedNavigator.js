916c0ab06491915bfd2d3d8b939a94f7d1d038d8
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-23 18:55:27 -0600

    create FeedNavigator

---
+++ app/navigation/FeedNavigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ListingDetailsScreen from "../screens/ListingDetailsScreen";
+import ListingsScreen from "../screens/ListingsScreen";
+
+const Stack = createStackNavigator();
+
+export default function FeedNavigator() {
+  <Stack.Navigator>
+    <Stack.Screen name="Listings" component={ListingsScreen} />
+    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
+  </Stack.Navigator>;
+}
