7c0d8c6a5dc82a75d5865b096ebfa4d90cfc6b41
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-23 22:26:26 -0600

    remove quotes and use localhost since IP address refuses connection

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: '"http://104.205.47.132:9000/api"',
+  baseURL: "http://localhost:9000/api",
 });
 
 export default apiClient;
