42bb8b81e9efc43e45d1aa0cd81edde2f30cef19
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-23 19:05:52 -0600

    display listing info where listing is indicated by route prop - components used as navigation screens also get route prop (as well as navigation prop) - this allows us to dynamically display listing info

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -4,13 +4,15 @@ import AppText from "../components/AppText";
 import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
-export default function ListingDetailsScreen(props) {
+export default function ListingDetailsScreen({ route }) {
+  const listing = route.params;
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <Image style={styles.image} source={listing.image} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>Red jacket for sale</AppText>
-        <AppText style={styles.price}>$100</AppText>
+        <AppText style={styles.title}>{listing.title}</AppText>
+        <AppText style={styles.price}>{listing.price}</AppText>
       </View>
       <View style={styles.userContainer}>
         <ListItem
