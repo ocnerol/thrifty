c8d9fca769a2f38e821c71fdb1a527d8185847a0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-25 16:23:00 -0600

    show useNetInfo hook vs the NetInfo.addEventListener method

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StyleSheet, View } from "react-native";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+  // the netInfo constant does both the subscribing and unsubscribing
+  // to changes in the network connection
+  // so we don't need to have a useEffect with cleanup
+  const netInfo = useNetInfo();
+
+  const unsubscribe = NetInfo.addEventListener((netInfo) =>
+    console.log(netInfo)
+  );
+
   return <View style={styles.container}></View>;
 }
 
