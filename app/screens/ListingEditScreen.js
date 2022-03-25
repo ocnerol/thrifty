cc856b619e2c2f21193145bd2ff2eb69bcce5d43
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-24 21:39:39 -0600

    always reset progress to 0 when submitting form (again)

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -85,6 +85,7 @@ export default function ListingEditScreen() {
   const [progress, setProgress] = useState(0);
 
   const handleSubmit = async (listing) => {
+    setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
       { ...listing, location },
