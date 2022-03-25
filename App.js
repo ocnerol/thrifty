db8114598eba5810a199dcd0ff4cbfdb3ef52e63
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-25 16:16:16 -0600

    clean up app.js

--- App.js
+++ App.js
@@ -1,71 +1,9 @@
-import React from "react";
-import { Button, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-
-import Screen from "./app/components/Screen";
-import navigationTheme from "./app/navigation/navigationTheme";
-import AppNavigator from "./app/navigation/AppNavigator";
-import AccountNavigator from "./app/navigation/AccountNavigator";
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const Link = () => {
-  const navigation = useNavigation();
-
-  return (
-    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
-  );
-};
-
-const Tweets = ({ navigation }) => {
-  return (
-    <Screen>
-      <Text>Tweets</Text>
-      <Button
-        title="View Tweet"
-        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-      />
-    </Screen>
-  );
-};
-
-const TweetDetails = () => {
-  return (
-    <Screen>
-      <Text>Tweet Details</Text>
-    </Screen>
-  );
-};
-
-const FeedNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Tweets" component={Tweets} />
-      <Stack.Screen name="TweetDetails" component={TweetDetails} />
-    </Stack.Navigator>
-  );
-};
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const TabNavigator = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
-  </Tab.Navigator>
-);
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: {},
+});
