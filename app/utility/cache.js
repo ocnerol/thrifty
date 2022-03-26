71e0c56a7adbd7b41205fe93a73fc417c197d278
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-25 18:24:20 -0600

    design function to retrieve data from cache

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,6 +1,8 @@
-import AsyncStorageLib from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import moment from "moment";
 
 const prefix = "cache";
+const expiryInMinutes = 5;
 
 const store = async (key, value) => {
   try {
@@ -14,6 +16,28 @@ const store = async (key, value) => {
   }
 };
 
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    const item = JSON.parse(value);
+
+    if (!item) return null;
+
+    const now = moment(Date.now());
+    const storedTime = moment(item.timestamp);
+    const isExpired = now.diff(storedTime, "minutes") > expiryInMinutes;
+
+    if (isExpired) {
+      await AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+    return item.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default {
+  get,
   store,
 };
