c5959974eef4ac2a5e07814f28e8d5f80f28927d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-03-25 18:33:13 -0600

    set up re-implementation of api GET method to allow for searching cache as well

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,17 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 const apiClient = create({
   baseURL: "http://192.168.1.75:9000/api",
 });
+
+// to change implementation of the GET method for making a request to the API:
+// e.g., to check the cache if there is no internet connection or if the request fails
+const get = apiClient.get;
+apiClient.get = () => {
+  // do something before
+  get();
+  // do something after
+};
 
 export default apiClient;
