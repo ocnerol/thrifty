116d5777cd78bfecd37d45655416d05091af2efb
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-17 19:05:23 -0700

    call handleSubmit which makes POST request to /auth endpoint when submitting form

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,6 +3,7 @@ import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import { Form, AppFormField, SubmitButton } from "../components/forms";
+import authApi from "../api/auth";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"), // label method takes label for displaying errors on this input field
@@ -10,12 +11,16 @@ const validationSchema = Yup.object().shape({
 });
 
 export default function LoginScreen() {
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
+  };
+
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       <Form
         initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <AppFormField
