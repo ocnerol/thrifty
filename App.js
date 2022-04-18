8f929f970cef4b08180a5e72f4189b3506024a96
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-18 13:11:05 -0700

    create user state variable

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useState } from "react";
+
 import OfflineNotice from "./app/components/OfflineNotice";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {/* <AppNavigator /> */}
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
