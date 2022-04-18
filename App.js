b891c89890f038b4e24965f2c66b467c281d459d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-18 15:05:00 -0700

    restore user info if was logged in before app refresh

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { useState } from "react";
+import jwtDecode from "jwt-decode";
 
 import OfflineNotice from "./app/components/OfflineNotice";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -9,6 +10,13 @@ import AuthContext from "./app/auth/context";
 
 export default function App() {
   const [user, setUser] = useState();
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+    setUser(jwtDecode(token));
+  };
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
