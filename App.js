e75d721235750d98f1737802510c28a1bab8319a
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-18 13:13:58 -0700

    add user and setUser to entire app context

--- App.js
+++ App.js
@@ -5,16 +5,17 @@ import OfflineNotice from "./app/components/OfflineNotice";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
   const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
         {/* <AppNavigator /> */}
         <AuthNavigator />
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
