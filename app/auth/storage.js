5243f6d95016741edfabd233c90b5256b703aef8
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-18 14:43:41 -0700

    define storeToken fn using SecureStore

---
+++ app/auth/storage.js
@@ -0,0 +1,11 @@
+import * as SecureStore from "expo-secure-store";
+
+const key = "authToken";
+
+const storeToken = async (authToken) => {
+  try {
+    await SecureStore.setItemAsync(key, authToken);
+  } catch (error) {
+    console.log("Error storing the auth token", error);
+  }
+};
