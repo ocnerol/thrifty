7499153fa9469adf6886489ac15cfd160f083bd1
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-05-23 14:58:56 -0600

    add logOut function to useAuth

--- app/auth/useAuth.js
+++ app/auth/useAuth.js
@@ -1,7 +1,15 @@
 import { useContext } from 'react';
+
 import AuthContext from './context';
+import authStorage from './storage';
 
 export default function useAuth() {
   const { user, setUser } = useContext(AuthContext);
-  return { user, setUser };
+
+  const logOut = () => {
+    setUser(null);
+    authStorage.removeToken();
+  };
+
+  return { user, logOut, setUser };
 }
