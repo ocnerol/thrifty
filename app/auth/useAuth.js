9727ba1dc7f5073d1f111018b2232458c66a0b29
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-05-23 15:08:15 -0600

    add logIn function and add to return object

--- app/auth/useAuth.js
+++ app/auth/useAuth.js
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode';
 import { useContext } from 'react';
 
 import AuthContext from './context';
@@ -6,10 +7,16 @@ import authStorage from './storage';
 export default function useAuth() {
   const { user, setUser } = useContext(AuthContext);
 
+  const logIn = (authToken) => {
+    const user = jwtDecode(authToken);
+    setUser(user);
+    authStorage.storeToken(authToken);
+  };
+
   const logOut = () => {
     setUser(null);
     authStorage.removeToken();
   };
 
-  return { user, logOut };
+  return { user, logIn, logOut };
 }
