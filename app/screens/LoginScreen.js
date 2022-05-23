c5b08add4ba98ea14f0815dd0b8632f9de1257e5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-05-23 15:08:32 -0600

    use logIn from useAuth hook

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,70 +1,64 @@
-import { useContext, useState } from "react";
-import { Image, StyleSheet } from "react-native";
-import * as Yup from "yup";
-import jwtDecode from "jwt-decode";
+import { useState } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import * as Yup from 'yup';
 
-import Screen from "../components/Screen";
+import Screen from '../components/Screen';
 import {
   ErrorMessage,
   Form,
   AppFormField,
   SubmitButton,
-} from "../components/forms";
-import authApi from "../api/auth";
-import AuthContext from "../auth/context";
-import authStorage from "../auth/storage";
+} from '../components/forms';
+import authApi from '../api/auth';
+import useAuth from '../auth/useAuth';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required().email().label("Email"), // label method takes label for displaying errors on this input field
-  password: Yup.string().required().min(4).label("Password"),
+  email: Yup.string().required().email().label('Email'), // label method takes label for displaying errors on this input field
+  password: Yup.string().required().min(4).label('Password'),
 });
 
 export default function LoginScreen() {
-  const authContext = useContext(AuthContext);
+  const { logIn } = useAuth();
   const [loginFailed, setLoginFailed] = useState(false);
 
   const handleSubmit = async ({ email, password }) => {
     const result = await authApi.login(email, password);
     if (!result.ok) return setLoginFailed(true);
     setLoginFailed(false);
-
-    const token = result.data;
-    const user = jwtDecode(token);
-    authContext.setUser(user);
-    authStorage.storeToken(token);
+    logIn(result.data);
   };
 
   return (
     <Screen style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
       <Form
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ email: '', password: '' }}
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <AppFormField
-          autoCapitalize="none"
+          autoCapitalize='none'
           autoCorrect={false}
-          icon="email"
-          keyboardType="email-address"
-          name="email"
-          placeholder="Email"
-          textContentType="emailAddress"
+          icon='email'
+          keyboardType='email-address'
+          name='email'
+          placeholder='Email'
+          textContentType='emailAddress'
         />
         <AppFormField
-          autoCapitalize="none"
+          autoCapitalize='none'
           autoCorrect={false}
-          icon="lock"
-          name="password"
-          placeholder="Password"
+          icon='lock'
+          name='password'
+          placeholder='Password'
           secureTextEntry
-          textContentType="password"
+          textContentType='password'
         />
         <ErrorMessage
-          error="Invalid email and/or password."
+          error='Invalid email and/or password.'
           visible={loginFailed}
         />
-        <SubmitButton title="Login" />
+        <SubmitButton title='Login' />
       </Form>
     </Screen>
   );
@@ -77,7 +71,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 80,
     height: 80,
-    alignSelf: "center",
+    alignSelf: 'center',
     marginTop: 50,
     marginBottom: 20,
   },
