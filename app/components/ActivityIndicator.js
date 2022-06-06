6baaeb061cb5ec37966504069c1a9a6024d7c10d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-06-06 13:31:20 -0600

    add overlay styling to activity indicator - the zIndex property makes it so that anything behind the activity indicator (such as the register button on the register screen, are no longer clickable or interactable since there is an element (the view with the overlay style) in front of them

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,13 +1,27 @@
-import LottieView from "lottie-react-native";
+import LottieView from 'lottie-react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function ActivityIndicator({ visible = false }) {
   if (!visible) return null;
 
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require("../assets/animations/loader.json")}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require('../assets/animations/loader.json')}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    opacity: 0.8,
+    height: '100%',
+    width: '100%',
+    zIndex: 1,
+  },
+});
