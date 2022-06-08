5cf912321492fd9e44a07515881cec664ba28344
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-06-08 12:18:41 -0600

    use navigationRef

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
+import { navigationRef } from './app/navigation/rootNavigation';
 
 export default function App() {
   const [user, setUser] = useState();
@@ -39,7 +40,7 @@ export default function App() {
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      <NavigationContainer theme={navigationTheme}>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
